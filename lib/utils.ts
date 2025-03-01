bbb1bf531d45f1f39bd190e52905f5b8e3242029
Chris Gala <cg@chargeholdings.co>
2025-03-01 01:35:49 -0800

    Add baseline components and car data

---
+++ lib/utils.ts
@@ -0,0 +1,21 @@
+import { clsx, type ClassValue } from "clsx";
+
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs);
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(price);
+}
+
+export const carMakes = [
+  "Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Buick", 
+  "Cadillac", "Chevrolet", "Chrysler", "Dodge", "Ferrari", "Fiat", "Ford", 
+  "Genesis", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", 
+  "Lamborghini", "Land Rover", "Lexus", "Lincoln", "Maserati", "Mazda", 
+  "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi", "Nissan", "Porsche", "Ram", 
+  "Rolls-Royce", "Subaru", "Tesla", "Toyota", "Volkswagen", "Volvo"
+];
